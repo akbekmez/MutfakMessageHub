c933212e3e680d4f7aecaff3510cd4a42f3828c7
akbekmez <ahmetakbekmez@gmail.com>
2025-12-07 00:35:01 +0300

    Delete docs/samples/samples directory

--- docs/samples/samples/samples/sample-requests.cs
+++
@@ -1,9 +0,0 @@
-public record GetWeatherQuery(string City) : IRequest<WeatherDto>;
-
-public class GetWeatherHandler : IRequestHandler<GetWeatherQuery, WeatherDto>
-{
-    public Task<WeatherDto> Handle(GetWeatherQuery req, CancellationToken token)
-    {
-        return Task.FromResult(new WeatherDto(req.City, 19));
-    }
-}
